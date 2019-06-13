0ba8d6b6674c85d35846c2c3cb7af380f6ecc68d
Megan Karabin <megankarabin@Megans-MacBook-Pro.local>
2019-06-13 10:40:41 -0400

    trial 👽

---
+++ pelicanconf.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+import os
+
+AUTHOR = 'Megan Karabin'
+SITENAME = 'Hydra'
+SITEURL = 'https://thelinguist12.github.io/my-blog'
+PATH = 'content'
+STATIC_PATHS = ['images']
+TIMEZONE = 'America/Toronto'
+DEFAULT_LANG = 'en'
+DEFAULT_PAGINATION = 10
+RELATIVE_URLS = True
